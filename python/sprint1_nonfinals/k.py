f44c80dc5d23bec8290259fa425e34d19a55aae5
Pavel Korsakov <PKorsakov@datafort.ru>
2023-08-22 12:18:05 +0700

    python 1j

--- python/sprint1_nonfinals/k.py
+++ python/sprint1_nonfinals/k.py
@@ -1,14 +1,20 @@
+# https://contest.yandex.ru/contest/23389/problems/K/
+
 from typing import List, Tuple
 
+
 def get_sum(number_list: List[int], k: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
+    j = int(''.join(map(str, number_list)))
+
+    return [int(x) for x in str(k + j)]
+
 
 def read_input() -> Tuple[List[int], int]:
     n = int(input())
     number_list = list(map(int, input().strip().split()))
     k = int(input())
     return number_list, k
+
 
 number_list, k = read_input()
 print(" ".join(map(str, get_sum(number_list, k))))
--- python/sprint1_nonfinals/l.py
+++ python/sprint1_nonfinals/l.py
@@ -1,13 +1,36 @@
+# https://contest.yandex.ru/contest/23389/problems/L/
+
+# L. Лишняя буква
+# Ограничение времени	1 секунда
+# Ограничение памяти	64Mb
+# Ввод	стандартный ввод или input.txt
+# Вывод	стандартный вывод или output.txt
+# Васе очень нравятся задачи про строки, поэтому он придумал свою. Есть 2 строки s и t, состоящие только из строчных
+# букв. Строка t получена перемешиванием букв строки s и добавлением 1 буквы в случайную позицию. Нужно найти
+# добавленную букву.
+#
+# Формат ввода
+# На вход подаются строки s и t, разделённые переносом строки. Длины строк не превосходят 1000 символов. Строки не
+# бывают пустыми.
+#
+# Формат вывода
+# Выведите лишнюю букву.
+
 from typing import Tuple
 
+
 def get_excessive_letter(shorter: str, longer: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+    anser = 0
+    for char in shorter + longer:
+        anser ^= ord(char)
+    return chr(anser)
+
 
 def read_input() -> Tuple[str, str]:
     shorter = input().strip()
     longer = input().strip()
     return shorter, longer
+
 
 shorter, longer = read_input()
 print(get_excessive_letter(shorter, longer))
